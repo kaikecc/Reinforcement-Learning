56427f2de422ab8fb739f6b725a8e0626410176d
kaikecc <kaike.castro@posgrad.ufsc.br>
2024-03-13 09:12:32 -0300

    very good

--- src/classes/_LoadInstances.py
+++ src/classes/_LoadInstances.py
@@ -1,12 +1,20 @@
-
 from pathlib import Path
 import numpy as np
 from datetime import datetime
-
+import pandas as pd  # Adicionado para melhorar a leitura de arquivos
 
 class LoadInstances:
-    def __init__(self, data_path):        
-        self.data_path = Path(data_path) 
+
+    '''
+    Exemplo de uso:
+    timestamp,P-PDG,P-TPT,T-TPT,P-MON-CKP,T-JUS-CKP,P-JUS-CKGL,T-JUS-CKGL,QGL,class
+    2017-02-01 02:02:07.000000,0.000000e+00,1.009211e+07,1.190944e+02,1.609800e+06,8.459782e+01,1.564147e+06,,0.000000e+00,0
+    2017-02-01 02:02:08.000000,0.000000e+00,1.009200e+07,1.190944e+02,1.618206e+06,8.458997e+01,1.564148e+06,,0.000000e+00,0
+    2017-02-01 02:02:09.000000,0.000000e+00,1.009189e+07,1.190944e+02,1.626612e+06,8.458213e+01,1.564148e+06,,0.000000e+00,0
+    2017-02-01 02:02:10.000000,0.000000e+00,1.009178e+07,1.190944e+02,1.635018e+06,8.457429e+01,1.564148e+06,,0.000000e+00,0
+    '''
+    def __init__(self, data_path):
+        self.data_path = Path(data_path)
         
     def class_and_file_generator(self, real=False, simulated=False, drawn=False):
         for class_path in self.data_path.iterdir():
@@ -14,75 +22,39 @@ class LoadInstances:
                 try:
                     class_code = int(class_path.stem)
                 except ValueError:
-                    # Se não for possível converter para int, pule este diretório
                     continue
 
                 for instance_path in class_path.iterdir():
-                    if (instance_path.suffix == '.csv'):
-                        if (simulated and instance_path.stem.startswith('SIMULATED')) or \
-                        (drawn and instance_path.stem.startswith('DRAWN')) or \
-                        (real and (not instance_path.stem.startswith('SIMULATED')) and \
-                        (not instance_path.stem.startswith('DRAWN'))):
+                    if instance_path.suffix == '.csv':
+                        prefix = instance_path.stem.split('_')[0]
+                        if ((simulated and prefix == 'SIMULATED') or 
+                            (drawn and prefix == 'DRAWN') or 
+                            (real and prefix not in ['SIMULATED', 'DRAWN'])):
                             yield class_code, instance_path
     
     def load_instance_with_numpy(self, events_names, columns):
+        well_names = [f'WELL-{i:05d}' for i in range(1, 19)]
+        real_instances = list(self.class_and_file_generator(real=True))
         
-        
-        well_names = [f'WELL-{i:05d}' for i in range(1, 19)]        
-        real_instances = list(self.class_and_file_generator(real=True, simulated=False, drawn=False))
+        arrays_list = []
 
-        arrays_list = []  # List to store processed NumPy arrays
-
-        for instance in real_instances:
-            class_code, instance_path = instance    
-            well, _ = instance_path.stem.split('_')  
+        for class_code, instance_path in real_instances:
+            well, _ = instance_path.stem.split('_')
+            
+            if class_code in events_names and well in well_names:
+                df = pd.read_csv(instance_path, usecols=columns + (['timestamp'] if 'timestamp' in columns else []))
+                df.replace([np.inf, -np.inf], np.nan, inplace=True)
+                df.dropna(inplace=True)
                 
-            if class_code in events_names.keys() and well in well_names:
-
-                # Read the entire CSV file into a NumPy array
-                with open(instance_path, 'r') as file:
-                    header = file.readline().strip().split(',')
-                    indices = [header.index(col) for col in columns]
-                                                    
-                    if 'timestamp' in columns:
-                        arr = np.genfromtxt(file, delimiter=',', usecols=indices[1:], dtype=np.float32)
-                        arr[np.isinf(arr)] = np.nan
-                        timestamp_idx = header.index('timestamp')
-                        # Primeiro, vamos carregar todos os timestamps como um array numpy
-                        file.seek(0)
-                        file.readline()  # Skip the header
-                        timestamps = np.genfromtxt(file, delimiter=',', skip_header=0, usecols=timestamp_idx, dtype=str)
-                        if isinstance(timestamps, str):
-                            timestamps = np.array([timestamps])
-
-                        # Converta os timestamps para o formato desejado antes de aplicar o filtro
-                        fmt = "%Y-%m-%d %H:%M:%S.%f"
-                        rounded_timestamps = np.array([datetime.strptime(ts, fmt).strftime("%Y-%m-%d %H:%M:%S") for ts in timestamps])
-
-                        # Agora, aplicamos o mesmo filtro usado em 'arr' para 'rounded_timestamps'
-                        # Precisamos determinar quais linhas em 'arr' NÃO serão removidas por conter NaN
-                        not_nan_rows = ~np.isnan(arr).any(axis=1)
-
-                        # Filtramos 'arr' e 'rounded_timestamps' usando este índice
-                        arr_filtered = arr[not_nan_rows]
-                        rounded_timestamps_filtered = rounded_timestamps[not_nan_rows]
-
-                        # Agora, ambos 'arr_filtered' e 'rounded_timestamps_filtered' estão alinhados
-                        # e podemos concatená-los sem enfrentar o problema de dimensões incompatíveis
-                        final_data = np.hstack([rounded_timestamps_filtered.reshape(-1, 1), arr_filtered])
-
-                        arrays_list.append(final_data)
-                    else:
-                        arr = np.genfromtxt(file, delimiter=',', usecols=indices, dtype=np.float32)
-                        # Tratando NaN e valores infinitos antes da conversão
-                        arr[np.isinf(arr)] = np.nan
-                        arr = arr[~np.isnan(arr).any(axis=1)]
-                        arr[:, :-1] = arr[:, :-1].astype(np.float32)  # Convert selected columns to float32
-                        arr[:, -1] = arr[:, -1].astype(np.int16)  # Convert 'class' column to int16
-                        
-                        arrays_list.append(arr)               
-
-        # Concatenate all processed NumPy arrays
+                if 'timestamp' in columns:
+                    df['timestamp'] = pd.to_datetime(df['timestamp']).dt.strftime("%Y-%m-%d %H:%M:%S")
+                    arr = df.to_numpy()
+                else:
+                    arr = df.to_numpy(dtype=np.float32)
+                    arr[:, -1] = arr[:, -1].astype(np.int16)
+                
+                arrays_list.append(arr)
+        
         final_array = np.concatenate(arrays_list) if arrays_list else np.array([])
-
+        
         return final_array
