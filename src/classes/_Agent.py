b8778e02318c12d6c2bed6816302343ee2c70985
kaikecc <kaike.castro@posgrad.ufsc.br>
2024-03-25 23:09:50 -0300

    Delete unused files and update model files

--- src/classes/_Agent.py
+++ src/classes/_Agent.py
@@ -150,7 +150,7 @@ class Agent:
     
     def launch_tensorboard(self):
         # Launch TensorBoard, and specify the log directory
-        logging.getLogger('tensorboard').setLevel(logging.WARNING)
+        logging.getLogger('tensorboard').setLevel(logging.ERROR)
         tb = tensorboard.program.TensorBoard()
         try:
             tb.configure(argv=[None, '--logdir', self.logdir, '--port', str(self.port)])
@@ -206,7 +206,7 @@ class Agent:
             exploration_initial_eps=1.0, # Taxa de exploração inicial
             exploration_final_eps=0.01, # Taxa de exploração final
             max_grad_norm=10, # O valor máximo para o recorte do gradiente
-            verbose=1,
+            verbose=0,
             tensorboard_log=self.logdir,  # Usa o caminho ajustado para os logs do TensorBoard            
             device='auto'
         )
@@ -214,16 +214,12 @@ class Agent:
         final_model_path = os.path.join(self.path_save, '_DQN')
         # Within env3W_dqn, before starting training
         top_score = [None]  # Supondo que top_score é gerenciado globalmente
-        tensorboard_callback = TensorboardCallback(model=model, referencia_top_score=top_score, caminho_salvar_modelo=final_model_path, verbose=1)
-        metrics_callback = MetricsCSVCallback(save_path=final_model_path, verbose=1)
+        tensorboard_callback = TensorboardCallback(model=model, referencia_top_score=top_score, caminho_salvar_modelo=final_model_path, verbose=0)
+        metrics_callback = MetricsCSVCallback(save_path=final_model_path, verbose=0)
         
-        # Treina o modelo
-        #TIMESTEPS = 10000  # Usando um número inteiro diretamente é mais claro
-        for i in range(1, 11):            
-            model.learn(total_timesteps=self.TIMESTEPS, log_interval = 4, reset_num_timesteps=False, tb_log_name="DQN", callback=[metrics_callback, tensorboard_callback])
-            # Ajuste para tornar o nome do arquivo salvo mais informativo
-            model_path = os.path.join(self.path_save, f'DQN_iteration_{i}_timesteps_{self.TIMESTEPS*i}')
-            model.save(model_path)
+               
+        model.learn(total_timesteps=self.TIMESTEPS, log_interval = 4, reset_num_timesteps=False, tb_log_name="DQN", callback=[metrics_callback, tensorboard_callback])
+            
                
         model.save(final_model_path)
         logging.info(f"Modelo final salvo em {final_model_path}")
@@ -283,7 +279,7 @@ class Agent:
 
         # Configura o modelo PPO
         model = PPO('MlpPolicy', 
-                    self.envs_train, verbose=1, 
+                    self.envs_train, verbose=0, 
                     learning_rate=1e-3, 
                     n_steps=1, 
                     batch_size=32, 
@@ -299,20 +295,14 @@ class Agent:
         # Callback personalizado para registrar recompensas no TensorBoard
         
         final_model_path = os.path.join(self.path_save, '_PPO')
-        metrics_callback = MetricsCSVCallback(save_path=final_model_path, verbose=1)
+        metrics_callback = MetricsCSVCallback(save_path=final_model_path, verbose=0)
 
         # Preparando callback de avaliação (descomente e ajuste conforme necessário)
         # eval_env = self.envs_random(dataset_test_scaled, 1)
         # eval_callback = EvalCallback(eval_env, best_model_save_path=checkpoint_dir, log_path=checkpoint_dir, eval_freq=5000, deterministic=True, render=False)
 
-        # Treina o modelo
-        #TIMESTEPS = 10000
-        for i in range(1, 11):
-            model.learn(total_timesteps=self.TIMESTEPS, reset_num_timesteps=False, tb_log_name="PPO", callback=[checkpoint_callback, metrics_callback])  # Adicione `eval_callback` à lista de callbacks, se estiver usando
-            model_path = os.path.join(checkpoint_dir, f'PPO_iteration_{i}_timesteps_{self.TIMESTEPS*i}')
-            model.save(model_path)
-            print(f"Modelo salvo em: {model_path}")
-
+        model.learn(total_timesteps=self.TIMESTEPS, reset_num_timesteps=False, tb_log_name="PPO", callback=[checkpoint_callback, metrics_callback])  # Adicione `eval_callback` à lista de callbacks, se estiver usando
+            
         # Salva o modelo final
         final_model_path = os.path.join(self.path_save, '_PPO')
         model.save(final_model_path)
@@ -382,7 +372,7 @@ class Agent:
         print(f"Para visualizar os logs do TensorBoard, execute:\ntensorboard --logdir='{self.logdir}'")
         logging.info(f"Para visualizar os logs do TensorBoard, execute:\ntensorboard --logdir='{self.logdir}'")
 
-        model = A2C('MlpPolicy', self.envs_train, verbose=1,
+        model = A2C('MlpPolicy', self.envs_train, verbose=0,
                     learning_rate=1e-3,
                     n_steps=1, # 5
                     gamma=0.99,
@@ -391,15 +381,11 @@ class Agent:
                     tensorboard_log=self.logdir)        
         
         final_model_path = os.path.join(self.path_save, '_A2C')
-        metrics_callback = MetricsCSVCallback(save_path=final_model_path, verbose=1)
+        metrics_callback = MetricsCSVCallback(save_path=final_model_path, verbose=0)
 
-        # Treina o modelo
-        #TIMESTEPS = 10000
-        for i in range(1, 11):
-            model.learn(total_timesteps=self.TIMESTEPS, reset_num_timesteps=False, tb_log_name="A2C", callback=[metrics_callback])
-            model_path = os.path.join(self.path_save, f'A2C_iteration_{i}_timesteps_{self.TIMESTEPS*i}')
-            model.save(model_path)
-
+       
+        model.learn(total_timesteps=self.TIMESTEPS, reset_num_timesteps=False, tb_log_name="A2C", callback=[metrics_callback])
+            
         # Salva o modelo final
         model.save(os.path.join(self.path_save, 'A2C'))
         logging.info(f"Modelo final salvo em {os.path.join(self.path_save, '_A2C')}")
@@ -476,9 +462,9 @@ class Agent:
         # Within env3W_dqn, before starting training
         top_score = [None]  # Use a list for mutability
         top_score = [None]  # Supondo que top_score é gerenciado globalmente
-        tensorboard_callback = TensorboardCallback(model=model_agent, referencia_top_score=top_score, caminho_salvar_modelo=final_model_path, verbose=1)
+        tensorboard_callback = TensorboardCallback(model=model_agent, referencia_top_score=top_score, caminho_salvar_modelo=final_model_path, verbose=0)
 
-        metrics_callback = MetricsCSVCallback(save_path=final_model_path, verbose=1)
+        metrics_callback = MetricsCSVCallback(save_path=final_model_path, verbose=0)
 
         # Cria o diretório se não existir
         if not os.path.exists(self.logdir):
